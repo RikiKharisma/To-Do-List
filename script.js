101f97247a3361f19b2be9127a835c5195c14b7b
Your Name <you@example.com>
2023-04-06 19:52:51 +0700

    to do list clear

---
+++ script.js
@@ -0,0 +1,32 @@
+document.querySelector("#push").onclick = function
+(){
+    if(document.querySelector("#newtask input").value.length == 0){
+        alert("Masukan Teks")
+    }else{
+        document.querySelector("#takes").innerHTML
+        +=` 
+         <div class="task">
+            <span id="taskname">${document.querySelector("#newtask input").value}</span>
+            <button class="delete">
+            <i class="fa-solid fa-trash" style="color: #0e1016;"></i>
+            </button>
+         </div>
+         
+         `
+
+         var current_tasks = document.querySelectorAll(".delete")
+         for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove()
+            }
+         }
+
+         var tasks = document.querySelectorAll(".task")
+         for(var i=0; i<current_tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed')
+            }
+         }
+         document.querySelector("#newtask input").value = ""
+    }
+}
